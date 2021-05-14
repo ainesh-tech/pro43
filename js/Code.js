4108653cfb1fbc403e88c8f8e24f3eb3469489a0
ainesh-tech <64733384+ainesh-tech@users.noreply.github.com>
2021-05-14 20:36:56 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,18 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+    text("ATADASBA" , 100 ,  250);
+    text("Hint : Stores Information" , 100 , 270);
+    text("CUTNIFON", 700, 150 ) ;
+    text("Hint : Performs something" , 700 ,170);
+    
+
+}
